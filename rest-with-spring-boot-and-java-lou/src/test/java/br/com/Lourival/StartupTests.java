07af8fbf30d320a50efd108c142d7a77fbfda2c8
Lourival Nascimento <lograveld@gmail.com>
2025-08-19 22:41:25 -0300

    First Steps in Java with Spring Boot 3.4.0

---
+++ rest-with-spring-boot-and-java-lou/src/test/java/br/com/Lourival/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.Lourival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
